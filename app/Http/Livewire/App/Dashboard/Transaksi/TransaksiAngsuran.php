f28f7dad1c7ff004834a09c9c23990119bdda8a5
indrakurnia123 <indrakurnia768@gmail.com>
2022-11-24 11:15:38 +0700

    add transaksi angsuran

---
+++ app/Http/Livewire/App/Dashboard/Transaksi/TransaksiAngsuran.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\App\Dashboard\Transaksi;
+
+use Livewire\Component;
+
+class TransaksiAngsuran extends Component
+{
+    public function render()
+    {
+        return view('livewire.app.dashboard.transaksi.transaksi-angsuran');
+    }
+}
