fb101d3e09e3958869912eddffd4ab20c489bd53
hirridwan <rmfhdofficial@gmail.com>
2022-11-24 09:44:09 +0700

    update dashboard

--- app/Http/Livewire/App/Dashboard/P2p/Component/NominalPembiayaan.php
+++ app/Http/Livewire/App/Dashboard/P2p/Component/NominalPembiayaan.php
@@ -18,8 +18,10 @@ class NominalPembiayaan extends Component
         ->join('finteches','datastudio_data.fintech_id','=','finteches.id')
         ->join('limit_kredit_fintech','datastudio_data.fintech_id','=','limit_kredit_fintech.fintech_id')
         ->select(DB::raw('
+        finteches.id as fintech_id,
         finteches.name as nama_fintech,
         finteches.logo as logo,
+        (select round(SUM(if(datastudio_data.status_pembiayaan=1,datastudio_data.nominal_pembiayaan,0))/limit_kredit_fintech.nominal*100,2)) as persen_limit,
         SUM(if(datastudio_data.status_pembiayaan=1,datastudio_data.nominal_pembiayaan,0)) AS nominal_aktif,
         SUM(if(datastudio_data.status_pembiayaan=2,datastudio_data.nominal_pembiayaan,0)) AS nominal_ditolak,
         SUM(if(datastudio_data.status_pembiayaan=3,datastudio_data.nominal_pembiayaan,0)) AS nominal_lunas,
@@ -60,6 +62,29 @@ class NominalPembiayaan extends Component
         ->where('status_pembiayaan',1)
         ->where('lokasi_perusahaan',2)
         ->get();
+
+        
+    }
+
+    public function getLimitFintech($fintechId)
+    {
+        $limitFintech = DB::table('datastudio_data')
+        ->join('finteches','datastudio_data.fintech_id','=','finteches.id')
+        ->join('limit_kredit_fintech','datastudio_data.fintech_id','limit_kredit_fintech.fintech_id')
+        ->select(DB::raw('
+        finteches.name,
+        finteches.logo,
+        SUM(if(datastudio_data.status_pembiayaan=1,datastudio_data.nominal_pembiayaan,0)) AS outstanding,
+        round(SUM(if(datastudio_data.status_pembiayaan=1,datastudio_data.nominal_pembiayaan,0))/limit_kredit_fintech.nominal*100,2) AS persen_outstanding,
+        limit_kredit_fintech.nominal AS nominal_limit,
+        limit_kredit_fintech.nominal-SUM(if(datastudio_data.status_pembiayaan=1,datastudio_data.nominal_pembiayaan,0)) AS sisa_limit,
+        100-round(SUM(if(datastudio_data.status_pembiayaan=1,datastudio_data.nominal_pembiayaan,0))/limit_kredit_fintech.nominal*100,2) AS sisa_persen
+        '))
+        ->where('fintech_id',$fintechId)
+        ->groupBy('name')
+        ->orderByDesc('outstanding')
+        ->get();
+        return $limitFintech;
     }
 
     public function render()
