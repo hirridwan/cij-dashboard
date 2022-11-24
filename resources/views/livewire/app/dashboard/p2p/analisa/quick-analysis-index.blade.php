3d7011a57a0726f4a3842b29950113531e8e697d
raihanadfal <raihanafdlhdi@gmail.com>
2022-11-24 13:08:01 +0700

    add form trx pelunasan

--- resources/views/livewire/app/dashboard/p2p/analisa/quick-analysis-index.blade.php
+++ resources/views/livewire/app/dashboard/p2p/analisa/quick-analysis-index.blade.php
@@ -1,3 +1,286 @@
-<div>
-    {{-- Care about people's approval and you will be their prisoner. --}}
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quick Analysis</title>
+    <style>
+        * {
+    margin: 0;
+    padding: 0;
+    font-size: 12pt;
+    font-family: Arial, Helvetica, sans-serif;
+    box-sizing: border-box;
+}
+
+@media screen {
+    .page {
+    width: 210mm;
+    display: flex;
+    flex-direction: column;
+    margin-top: 50px;
+    box-shadow: inset;
+    /* border: 1px solid black; */
+    /* border-radius: 8px; */
+    width: 210mm;
+    max-width: 210mm;   
+    padding: 10mm;
+    margin: 10mm auto;
+    border: 1px #D3D3D3 solid;
+    border-radius: 5px;
+    background: white;
+    box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+    position: relative;
+}
+
+.header {
+    text-align: center;
+}
+
+/* table styling */
+table {
+    margin: 18px 0;
+    width: 100%;
+    padding: 8px;
+    border-collapse: collapse;
+}
+
+tr:nth-child(even) {
+    background-color: #eaeaea;
+}
+
+th {
+    background-color: #dedede;
+}
+
+tr, td, th {
+    text-align: left;
+    margin: 8px;
+    padding: 8px;
+}  
+
+
+}
+@media print {
+    .page {
+        font-size: 12pt;
+        width: 210mm;
+        margin: auto;
+    }
+    .header {
+    text-align: center;
+}
+
+/* table styling */
+table {
+    margin: 18px 0;
+    width: 100%;
+    padding: 8px;
+    border-collapse: collapse;
+}
+
+tr:nth-child(even) {
+    background-color: #eaeaea;
+}
+
+th {
+    background-color: #dedede;
+}
+
+tr, td, th {
+    text-align: left;
+    margin: 8px;
+    padding: 8px;
+}  
+
+}
+    </style>
+</head>
+<body>
+
+    <div class="page">
+        <div class="header">
+            <div class="row">
+                <div class="col">
+                    <div class="main-header">
+                        <h2>QUICK ANALYSIS PEMBIAYAAN FINTECH</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row" style="padding: 6px;">
+                <div class="col">
+                    <div class="tittle">
+                        <h4>No. Factsheet</h4>
+                    </div>
+                    <div class="letter-code">
+                        <h4>UKM-1221-0842</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="body">
+            <div class="row">
+                <div class="col">
+                    <table>
+                        <tr>
+                            <td width="200">Nama Fintech</td>
+                            <td>:</td>
+                            <td>PT Alim Rugi</td>
+                            <td>Fasilitas Aktif Fintech</td>
+                        </tr>
+                        <tr>
+                            <td>Nama Borrower</td>
+                            <td>:</td>
+                            <td>Raihan Afdal Hadi</td>
+                            <td rowspan="2">Rp.6.500.000</td>
+                        </tr>
+                        <tr>
+                            <td>Plafon Penawaran</td>
+                            <td>:</td>
+                            <td>Rp. 6.500.000</td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <table>
+                        <tr>
+                            <th>% Komitmen Pemberian Kredit</th>
+                            <th>Nominal Fasilitas Aktif</th>
+                            <th>Nominal Fasilitas Aktif</th>
+                        </tr>
+                        <tr>
+                            <td>99.4%</td>
+                            <td>Rp. 6.500.000</td>
+                            <td>99.4%</td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <table>
+                        <tr>
+                            <th colspan="2">A. Risk Acceptance Criteria(RAC)</th>
+                            <th width="200">Score</th>
+                        </tr>
+                        <tr>
+                            <td width="500">Lokasi Project</td>
+                            <td width="400">Luar Jawa Barat</td>
+                            <td>5.1</td>
+                        </tr>
+                        <tr>
+                            <td>Lokasi Perusahaan</td>
+                            <td>Luar Jawa Barat</td>
+                            <td>5.200</td>
+                        </tr>
+                        <tr>
+                            <td>Bentuk Peminjam</td>
+                            <td>Settlement Financing</td>
+                            <td>52.300</td>
+                        </tr>
+                        <tr>
+                            <td>Bentuk Peminjam</td>
+                            <td>Settlement Financing</td>
+                            <td>52.300</td>
+                        </tr>
+                        <tr>
+                            <td>Kriteria Borrower</td>
+                            <td>Badan Hukum</td>
+                            <td>100</td>
+                        </tr>
+                        <tr>
+                            <td colspan="2">Total Score</td>
+                            <td>24.500</td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <table>
+                        <tr>
+                            <th colspan="2">B. Fitur Pendanaan Pinjaman</th>
+                            <th width="200">Score</th>
+                        </tr>
+                        <tr>
+                            <td width="500">Skema</td>
+                            <td width="400">Luar Jawa Barat</td>
+                            <td>100</td>
+                        </tr>
+                        <tr>
+                            <td>Tujuan</td>
+                            <td>Uang Kuliah</td>
+                            <td>100</td>
+                        </tr>
+                        <tr>
+                            <td>Suku Bunga</td>
+                            <td>25.000%</td>
+                            <td>100</td>
+                        </tr>
+                        <tr>
+                            <td>Sifat</td>
+                            <td>installment</td>
+                            <td>100</td>
+                        </tr>
+                        <tr>
+                            <td>Rating Borrower</td>
+                            <td>B+</td>
+                            <td>52.800</td>
+                        </tr>
+                        <tr>
+                            <td>Jangka Waktu</td>
+                            <td>4 Hari</td>
+                            <td>Agunan Utama</td>
+                        </tr>
+                        <tr>
+                            <td>Agunan Utama</td>
+                            <td>Invoice</td>
+                            <td>46.300</td>
+                        </tr>
+                        <tr>
+                            <td>Rating Pefindo</td>
+                            <td>Tidak ada</td>
+                            <td>700</td>
+                        </tr>
+                        <tr>
+                            <td colspan="2">Total Score</td>
+                            <td>24.800</td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <table>
+                        <tr>
+                            <th colspan="2">C. Payor Analisis</th>
+                            <th>High Risk Pefindo</th>
+                        </tr>
+                        <tr>
+                            <td width="300">Total Payor</td>
+                            <td>52.5</td>
+                            <td rowspan="2">98.46%</td>
+                        </tr>
+                        <tr>
+                            <td>Payor Scoring</td>
+                            <td>52.500</td>
+                        </tr>
+                        <tr>
+                            <td style="background-color: #dedede; font-weight: bold;">RAC Scoring</td>
+                            <td style="background-color: #dedede; font-weight: bold;"">42.00</td>
+                            <td rowspan="2" style="text-align: center; background-color: #D8FDDA;">Payor Layak</td>
+                        </tr>
+                        <tr>
+                            <td colspan="2">Rp. 5.550.000</td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
