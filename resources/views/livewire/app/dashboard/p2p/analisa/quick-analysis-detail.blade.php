828c6bec42c373a3bf0e776174f15cb3c68bdcdc
raihanadfal <raihanafdlhdi@gmail.com>
2022-11-28 09:27:50 +0700

    edit quick analysis

--- resources/views/livewire/app/dashboard/p2p/analisa/quick-analysis-detail.blade.php
+++ resources/views/livewire/app/dashboard/p2p/analisa/quick-analysis-detail.blade.php
@@ -71,7 +71,7 @@
             }
             
             @media print {
-                .page {
+                .container {
                     font-size: 12pt;
                     width: 210mm;
                     margin: auto;
@@ -106,160 +106,173 @@
             }
     </style>
     @endpush
-    
-    <div class="header">
-        <div class="row">
-            <div class="col">
-                <div class="main-header">
-                    <div class="row-header" style="display: flex; flex-direction: row; justify-items: space-between;">
-                        <div class="col">
-                            <img src="https://bankcij.com/wp-content/uploads/2021/08/cropped-LOGO-CIJ-mini-e1630984123700.png" alt="">
+
+    <div class="container">
+        <div class="header">
+            <div class="row">
+                <div class="col">
+                    <div class="main-header">
+                        <div class="row-header" style="display: flex; flex-direction: row; justify-items: space-between;">
+                            <div class="col">
+                                <img src="https://bankcij.com/wp-content/uploads/2021/08/cropped-LOGO-CIJ-mini-e1630984123700.png" alt="">
+                            </div>
+                            <div class="col"></div>
+                            <div class="col">
+                                <img src="{{asset('images/p2p.png')}}" alt="logo"/>
+                            </div>
                         </div>
-                        <div class="col"></div>
-                        <div class="col">
-                            <img src="{{asset('images/p2p.png')}}" alt="logo"/>
+                        <div class="row" style="margin-top: 20px;">
+                            <div class="col">
+                                <h2>QUICK ANALYSIS PEMBIAYAAN FINTECH</h2>
+                            </div>
                         </div>
                     </div>
-                    <div class="row" style="margin-top: 20px;">
-                        <div class="col">
-                            <h2>QUICK ANALYSIS PEMBIAYAAN FINTECH</h2>
-                        </div>
+                </div>
+            </div>
+            <div class="row" style="padding: 6px;">
+                <div class="col">
+                    <div class="tittle">
+                        <h4>No. Factsheet</h4>
+                    </div>
+                    <div class="letter-code">
+                        <h4 style="font-weight: bold; font-size: 16pt">{{$analysis->no_factsheet}}</h4>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="row" style="padding: 6px;">
-            <div class="col">
-                <div class="tittle">
-                    <h4>No. Factsheet</h4>
+        <div class="body">
+            <div class="row">
+                <div class="col">
+                    <table>
+                        <tr>
+                            <td style="font-weight: bold;">Nama Fintech</td>
+                            <td>:</td>
+                            <td>{{$analysis->nama_fintech}}</td>
+                        </tr>
+                        <tr style="background-color: #fff">
+                            <td style="font-weight: bold;">Nama Borrower</td>
+                            <td>:</td>
+                            <td>{{$analysis->nama_borrower}}</td>
+                        </tr>
+                        <tr>
+                            <td style="font-weight: bold;">Plafon Penawaran</td>
+                            <td>:</td>
+                            <td>{{number_format($analysis->plafon_penawaran,0,',','.')}}</td>
+                        </tr>
+                    </table>
                 </div>
-                <div class="letter-code">
-                    <h4 style="font-weight: bold; font-size: 16pt">{{$analysis->no_factsheet}}</h4>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <table>
+                        <tr>
+                            <th>Fasilitas Aktif Fintech</th>
+                        </tr>
+                        <td>
+                            {{number_format($analysis->fasilitas_aktif_fintech,0,',','.')}}
+                        </td>
+                    </table>
                 </div>
             </div>
-        </div>
-    </div>
-    <div class="body">
-        <div class="row">
-            <div class="col">
-                <table>
-                    <tr>
-                        <td style="font-weight: bold;">Nama Fintech</td>
-                        <td>:</td>
-                        <td>{{$analysis->nama_fintech}}</td>
-                    </tr>
-                    <tr style="background-color: #fff">
-                        <td style="font-weight: bold;">Nama Borrower</td>
-                        <td>:</td>
-                        <td>{{$analysis->nama_borrower}}</td>
-                    </tr>
-                    <tr>
-                        <td style="font-weight: bold;">Plafon Penawaran</td>
-                        <td>:</td>
-                        <td>{{number_format($analysis->plafon_penawaran,0,',','.')}}</td>
-                    </tr>
-                </table>
+            <div class="row">
+                <div class="col">
+                    <table>
+                        <tr>
+                            <th>% Komitmen Pemberian Kredit</th>
+                            <th>Nominal Fasilitas Aktif</th>
+                            <th>NOA Fasilitas Aktif</th>
+                        </tr>
+                        <tr>
+                            <td>{{$analysis->komitmen_pemberian_kredit}}%</td>
+                            <td>{{number_format($analysis->nominal_fasilitas_aktif,2,',','.')}}</td>
+                            <td>{{number_format($analysis->noa_fasilitas_aktif,2,',','.')}}</td>
+                        </tr>
+                    </table>
+                </div>
             </div>
-        </div>
-        <div class="row">
-            <div class="col">
-                <table>
-                    <tr>
-                        <th>Fasilitas Aktif Fintech</th>
-                    </tr>
-                    <td>
-                        {{number_format($analysis->fasilitas_aktif_fintech,0,',','.')}}
-                    </td>
-                </table>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col">
-                <table>
-                    <tr>
-                        <th>% Komitmen Pemberian Kredit</th>
-                        <th>Nominal Fasilitas Aktif</th>
-                        <th>NOA Fasilitas Aktif</th>
-                    </tr>
-                    <tr>
-                        <td>{{$analysis->komitmen_pemberian_kredit}}%</td>
-                        <td>{{number_format($analysis->nominal_fasilitas_aktif,2,',','.')}}</td>
-                        <td>{{number_format($analysis->noa_fasilitas_aktif,2,',','.')}}</td>
-                    </tr>
-                </table>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col">
-                <table>
-                    <tr>
-                        <th colspan="2">A. Risk Acceptance Criteria(RAC)</th>
-                        <th width="200">Score</th>
-                    </tr>
-                    @foreach ($score_rac_detail as $item)
-                    <tr>
-                        <td width="500">{{ucfirst(str_replace('_',' ',$item->parameter))}}</td>
-                        <td width="400">{{$item->value}}</td>
-                        <td>{{$item->score}}</td>
-                    </tr>                        
-                    @endforeach
-                    <tr>
-                        <td colspan="2" style="font-weight: bold; text-align: center;">Total Score</td>
-                        <td><b>{{$score_rac[0]->score}}</b></td>
-                    </tr>
-                </table>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col">
-                <table>
-                    <tr>
-                        <th colspan="2">B. Fitur Pendanaan Pinjaman</th>
-                        <th width="200">Score</th>
-                    </tr>
-                    @foreach ($score_fitur_pendanaan_detail as $item)
+            <div class="row">
+                <div class="col">
+                    <table>
+                        <tr>
+                            <th colspan="2">A. Risk Acceptance Criteria(RAC)</th>
+                            <th width="200">Score</th>
+                        </tr>
+                        @foreach ($score_rac_detail as $item)
                         <tr>
                             <td width="500">{{ucfirst(str_replace('_',' ',$item->parameter))}}</td>
                             <td width="400">{{$item->value}}</td>
                             <td>{{$item->score}}</td>
+                        </tr>                        
+                        @endforeach
+                        <tr>
+                            <td colspan="2" style="font-weight: bold; text-align: center;">Total Score</td>
+                            <td><b>{{$score_rac[0]->score}}</b></td>
                         </tr>
-                    @endforeach
-                    <tr>
-                        <td colspan="2" style="font-weight: bold; text-align: center;">Total Score</td>
-                        <td><b>{{$score_fitur_pendanaan[0]->score}}</b></td>
-                    </tr>
-                </table>
+                    </table>
+                </div>
             </div>
-        </div>
-        <div class="row">
-            <div class="col">
-                <table>
-                    <tr>
-                        <th colspan="2">C. Payor Analisis</th>
-                        <th class="text-center @if($analysis->pefindo_status=='HIGH RISK PEFINDO') bg-danger text-white @else bg-success text-white @endif">{{$analysis->pefindo_status}}</th>
-                    </tr>
-                    <tr>
-                        <td width="300">Total Payor</td>
-                        <td>{{$analysis->total_payor}}</td>
-                        <td rowspan="2" class="text-center">{{$analysis->pefindo_score}}</td>
-                    </tr>
-                    <tr>
-                        <td>Payor Scoring</td>
-                        <td>{{$analysis->score_payor}}</td>
-                    </tr>
-                    <tr>
-                        <td style="background-color: #6c7ae0; font-weight: bold; color: #fff;">RAC Scoring</td>
-                        <td style="background-color: #6c7ae0; font-weight: bold; color: #fff; text-align: right;">{{$analysis->max_pembiayaan_persen}}</td>
-                        <td rowspan="2" class="@if($analysis->kelayakan_payor !='Payor Layak') text-danger @endif" style="text-align: center; background-color: #D8FDDA;"><b>{{$analysis->kelayakan_payor}}</b></td>
-                    </tr>
-                    <tr>
-                        <td align="center" style="background-color: #f8f6ff;">Rekomendasi Pembiayaan</td>
-                        <td align="center" style="background-color: #f8f6ff;">{{number_format($analysis->nominal_rekomendasi,0,',','.')}}</td>
-                    </tr>
-                </table>
+            <div class="row">
+                <div class="col">
+                    <table>
+                        <tr>
+                            <th colspan="2">B. Fitur Pendanaan Pinjaman</th>
+                            <th width="200">Score</th>
+                        </tr>
+                        @foreach ($score_fitur_pendanaan_detail as $item)
+                            <tr>
+                                <td width="500">{{ucfirst(str_replace('_',' ',$item->parameter))}}</td>
+                                <td width="400">{{$item->value}}</td>
+                                <td>{{$item->score}}</td>
+                            </tr>
+                        @endforeach
+                        <tr>
+                            <td colspan="2" style="font-weight: bold; text-align: center;">Total Score</td>
+                            <td><b>{{$score_fitur_pendanaan[0]->score}}</b></td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <table>
+                        <tr>
+                            <th colspan="2">C. Payor Analisis</th>
+                            <th class="text-center @if($analysis->pefindo_status=='HIGH RISK PEFINDO') bg-danger text-white @else bg-success text-white @endif">{{$analysis->pefindo_status}}</th>
+                        </tr>
+                        <tr>
+                            <td width="300">Total Payor</td>
+                            <td>{{$analysis->total_payor}}</td>
+                            <td rowspan="2" class="text-center">{{$analysis->pefindo_score}}</td>
+                        </tr>
+                        <tr>
+                            <td>Payor Scoring</td>
+                            <td>{{$analysis->score_payor}}</td>
+                        </tr>
+                        <tr>
+                            <td style="background-color: #6c7ae0; font-weight: bold; color: #fff;">RAC Scoring</td>
+                            <td style="background-color: #6c7ae0; font-weight: bold; color: #fff; text-align: right;">{{$analysis->max_pembiayaan_persen}}</td>
+                            <td rowspan="2" class="@if($analysis->kelayakan_payor !='Payor Layak') text-danger @endif" style="text-align: center; background-color: #D8FDDA;"><b>{{$analysis->kelayakan_payor}}</b></td>
+                        </tr>
+                        <tr>
+                            <td align="center" style="background-color: #f8f6ff;">Rekomendasi Pembiayaan</td>
+                            <td align="center" style="background-color: #f8f6ff;">{{number_format($analysis->nominal_rekomendasi,0,',','.')}}</td>
+                        </tr>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
 
-
 </div>
+
+<div class="row w-75 text-right" style="margin: auto;">
+    <div class="col">
+        <div class="btn-group">
+            <div class="btn btn-primary">
+                Cetak PDF
+            </div>
+        </div>
+    </div>
+</div>
+
+
--- resources/views/livewire/app/dashboard/transaksi/transaksi-angsuran.blade.php
+++ resources/views/livewire/app/dashboard/transaksi/transaksi-angsuran.blade.php
@@ -1,90 +1,99 @@
-    <div class="container w-75 mt-4">
-        <div class="row">
-            <div class="col">
-                <div class="card">
-                    <div class="card-header bg-primary text-white text-center">
-                        <h4>Transaksi Pelunasan</h4>
-                    </div>
-                    <div class="card-body">
-                        <form>
-                            <div class="row">
-                                <div class="col">
-                                    <div class="form-group">
-                                        <label>Nomor Rekening</label>
-                                        <input type="text" class="form-control">
-                                    </div>
+<div class="container w-75 mt-4">
+    <div class="row">
+        <div class="col">
+            <div class="card">
+                <div class="card-header bg-primary text-white text-center">
+                    <h4>Transaksi Pelunasan</h4>
+                </div>
+                <div class="card-body">
+                    <form>
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <div class="form-group">
+                                    <label>Nama Fintech</label>
+                                    <select class="form-control">
+                                        <option value="1">-- Nama Fintech --</option>
+                                    </select>
                                 </div>
                             </div>
-                            <div class="row">
-                                <div class="col">
-                                    <div class="form-group my-0">
-                                        <input type="text" readonly class="form-control-plaintext" value="Nama Borrower">
-                                    </div>
-                                </div>
-                                <div class="col">
-                                    <div class="form-group my-0">
-                                        <input type="text" readonly class="form-control-plaintext" value="Nomor Factsheet">
-                                    </div>
+                            <div class="col-sm-6">
+                                <div class="form-group">
+                                    <label>Nama Borrower</label>
+                                    <input type="text" class="form-control">
                                 </div>
                             </div>
-                            <hr>
-                            <div class="row">
-                                <div class="col-sm-6">
-                                    <div class="form-group">
-                                        <label>Kode Transaksi</label>
-                                        <input type="text" class="form-control">
-                                    </div>
-                                </div>
-                                <div class="col-sm-6">
-                                    <div class="form-group">
-                                        <label>Tanggal Transaksi</label>
-                                        <input type="date" class="form-control datetimepicker">
-                                    </div>
+                            <div class="col-sm-6">
+                                <div class="form-group">
+                                    <label>Nomor Rekening</label>
+                                    <input type="text" class="form-control">
                                 </div>
                             </div>
-                            <div class="row">
-                                <div class="col-sm-6">
-                                    <div class="form-group">
-                                        <label>Pokok</label>
-                                        <input type="text" class="form-control">
-                                    </div>
-                                </div>
-                                <div class="col-sm-6">
-                                    <div class="form-group">
-                                        <label>Bunga</label>
-                                        <input type="text" class="form-control">
-                                    </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <div class="form-group my-0">
+                                    <input type="text" readonly class="form-control-plaintext" value="Nama Borrower">
                                 </div>
                             </div>
-                            <div class="row">
-                                <div class="col-sm-6">
-                                    <div class="form-group">
-                                        <label>Diskon Bunga</label>
-                                        <input type="text" class="form-control">
-                                    </div>
-                                </div>
-                                <div class="col-sm-6">
-                                    <div class="form-group">
-                                        <label>Denda</label>
-                                        <input type="text" class="form-control">
-                                    </div>
+                            <div class="col">
+                                <div class="form-group my-0">
+                                    <input type="text" readonly class="form-control-plaintext" value="Nomor Factsheet">
                                 </div>
                             </div>
-                            <div class="row">
-                                <div class="col text-right">
-                                    <button class="btn-primary btn">Simpan</button>
+                        </div>
+                        <hr>
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <div class="form-group">
+                                    <label>Kode Transaksi</label>
+                                    <select class="form-control">
+                                        <option value="">-- Pilih Kode Trx --</option>
+                                    </select>
                                 </div>
                             </div>
-                        </form>
-                    </div>
+                            <div class="col-sm-6">
+                                <div class="form-group">
+                                    <label>Tanggal Transaksi</label>
+                                    <input type="date" class="form-control datetimepicker">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <div class="form-group">
+                                    <label>Pokok</label>
+                                    <input type="text" class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="form-group">
+                                    <label>Bunga</label>
+                                    <input type="text" class="form-control">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <div class="form-group">
+                                    <label>Diskon Bunga</label>
+                                    <input type="text" class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="form-group">
+                                    <label>Denda</label>
+                                    <input type="text" class="form-control">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col text-right">
+                                <button class="btn-primary btn">Simpan</button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
     </div>
-
-    <!-- General JS Scripts -->
-    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.nicescroll/3.7.6/jquery.nicescroll.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
+</div>
